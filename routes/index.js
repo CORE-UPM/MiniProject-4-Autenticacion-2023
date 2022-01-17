22f097255422d1a4865fae15af184fffe6baed0a
Santiago Pavon <santiago.pavon@upm.es>
2022-01-17 12:57:14 +0100

    Local Authentication.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const sessionController = require('../controllers/session');
+
+//-----------------------------------------------------------
+
+// autologout
+router.all('*',sessionController.deleteExpiredUserSession);
+
+//-----------------------------------------------------------
+
+// Routes for the resource /session
+router.get('/login',    sessionController.new);     // login form
+router.post('/login',   sessionController.create);  // create sesion
+router.delete('/login', sessionController.destroy); // close sesion
+
+//-----------------------------------------------------------
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/users');
